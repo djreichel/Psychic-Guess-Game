37db3048b6fa85615d76b14fd03ceaf6c5dd380e
picasso <46543347+djreichel@users.noreply.github.com>
2019-02-13 14:19:56 -0500

    first update

--- game.js
+++ game.js
@@ -22,7 +22,9 @@ var updateGuessesLeft = function() {
 };
 
 var updateLetterToGuess = function() {
+  
   //*** Here we get a random letterToGuess and assign it based on a random generator - use math to set letterToGuess equal to a random letter from our letters array (hints - math.floor, math.random will be used and letters array will be referenced)
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
   //========================================== letterToGuess = 
 };
 
@@ -33,32 +35,62 @@ var updateGuessesSoFar = function() {
 
 // Function will be called when we reset everything
 var reset = function() {
+
   //***Hardcode reset guessesLeft
+  guessesLeft = 9;
   //==========================================
+
   //***hardcode reset guessedLetters
+  guessedLetters = [];
   //==========================================
+
   //***call function to reset letter to guess
+  document.getElementById("letters");
+
   //==========================================
+
   //***call function to update guesses left in html
+  document.getElementById("Guesses-left").textContent = "Guesses Left: ";
   //==========================================
+
   //***call function to update guesses made so far in html
+  document.getElementById("guesses-so-far").textContent = "Your Guesses so far: ";
   //==========================================
 };
 
 // Execute on page load.
 //***call function to update letter to guess
+document.onload = function(){
+
+  var updateLettersToGuess = function() {
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
 //==========================================
+
 //***call function to update guesses left
+var updateGuessesLeft = function () {
+  document.getElementById("Guesses-left").textContent = "Guesses Left: ";
+}
 //==========================================
+}
 
 // This function will capture the keyboard clicks.
 document.onkeydown = function(event) {
+
   //***subtract one from guesses left when a key is clicked
+  for (var i=0; i=guessesLeft.length; i--) {
+    document.getElementById("Guesses-left").textContent = "Guesses Left: ";
+  }
   //==========================================
+
   // Lowercase the letter
   var letter = event.key.toLowerCase();
 
   // ***Add the guess to the guessedLetters array using .push
+  for (var i=0; i<letters.length; i++) {
+    var letterGuess = ["guessedLetters"];
+      document.getElementById("letters").push = "guesses-so-far: ";
+  }
   //==========================================
 
   // Then its going to run the update functions
